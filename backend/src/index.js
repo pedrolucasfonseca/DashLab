3f3f65d613223ffab7e97a6fae9c85ab0f15886a
Pedro Lucas <pedrolucasfonseca98@gmail.com>
2026-05-31 13:20:13 -0300

    feat(jest): adiciona o jest para automatizar testes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-require('dotenv').config();
+const app = require('./app');
 
-const healthRoutes = require('./routes/health');
-const apiRoutes = require('./routes/api');
+const PORT = process.env.PORT || 3001;
 
-const app = express();
-const PORT =  process.env.PORT || 3001;
-
-app.use(helmet());
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-
-app.use('/health', healthRoutes);
-app.use('/api', apiRoutes);
-
-app.listen(PORT, () => {console.log(`DashLab (BackEnd) rodando na porta ${PORT}`)});
-
-module.exports = app;
+app.listen(PORT, () => {
+	console.log(`DashLab (backend) rodando na porta ${PORT}`);
+});
