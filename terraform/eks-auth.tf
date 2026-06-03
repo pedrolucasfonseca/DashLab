7b53479be4fb0b666edbc8d59d419e24e7165bba
Pedro Lucas <pedrolucasfonseca98@gmail.com>
2026-06-02 22:35:40 -0300

    fix(terraform): Torna o ARN do admin da EKS configurável e diminui o escopo da CI

--- terraform/eks-auth.tf
+++ terraform/eks-auth.tf
@@ -1,29 +1,30 @@
 resource "aws_eks_access_entry" "github_actions" {
-  cluster_name  = aws_eks_cluster.main.name
+  cluster_name = aws_eks_cluster.main.name
   principal_arn = aws_iam_role.github_actions.arn
-  type          = "STANDARD"
+  type = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github_actions" {
-  cluster_name  = aws_eks_cluster.main.name
+  cluster_name = aws_eks_cluster.main.name
   principal_arn = aws_iam_role.github_actions.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
 
   access_scope {
-    type = "cluster"
+    type = "namespace"
+    namespaces = ["dashlab"]
   }
 }
 
 resource "aws_eks_access_entry" "admin" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = "arn:aws:iam::496577768562:user/dashlab-admin"
-  type          = "STANDARD"
+  cluster_name = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::${var.aws_account_id}:user/dashlab-admin"
+  type = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "admin" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = "arn:aws:iam::496577768562:user/dashlab-admin"
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  cluster_name = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::${var.aws_account_id}:user/dashlab-admin"
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
